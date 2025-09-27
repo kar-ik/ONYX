377e8c4733aafea0673cf0c75c828414390e1f85
kk <112804222+kar-ik@users.noreply.github.com>
2025-09-27 22:39:01 +0530

    Update __init__.py

--- onyx/adapters/__init__.py
+++ onyx/adapters/__init__.py
@@ -2,10 +2,17 @@ from .google import GoogleAdapter
 from .hibp import HIBPAdapter
 
 def get_adapters(sources=None, config=None):
-    all_adapters = [
-        GoogleAdapter(config.get('GOOGLE_API_KEY'), config.get('GOOGLE_CX')),
-        HIBPAdapter(),
-    ]
+    all_adapters = []
+
+    if config.get("GOOGLE_API_KEY") and config.get("GOOGLE_CX"):
+        all_adapters.append(
+            GoogleAdapter(config.get("GOOGLE_API_KEY"), config.get("GOOGLE_CX"))
+        )
+
+    if config.get("HIBP_API_KEY"):
+        all_adapters.append(HIBPAdapter(config.get("HIBP_API_KEY")))
+
     if sources:
         return [a for a in all_adapters if a.name in sources]
+
     return all_adapters
